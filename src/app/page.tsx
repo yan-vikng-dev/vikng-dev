bf35edf109c07ffa55e9b72e1a0669f22ed94746
yan-vikng-dev <yan@vikng.dev>
2025-09-14 00:29:12 +0700

    test deploy 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              test deployment
+              test deployment 2
             </code>
             .
           </li>
