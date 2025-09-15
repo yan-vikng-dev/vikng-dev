3b86dadf0d4ce40669a3a854a9b60eeae39159f7
yan-vikng-dev <yan@vikng.dev>
2025-09-15 11:54:17 +0700

    lint

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  distDir: process.env.NODE_ENV === "development" ? ".next-dev" : ".next",
   /* config options here */
 };
 
